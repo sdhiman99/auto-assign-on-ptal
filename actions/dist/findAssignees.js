a82c87ad615279daedfa990245756c07a39a85c6
shivansh <shivanshdhiman@gmail.com>
2020-03-22 19:04:17 +0530

    ssdf

---
+++ actions/dist/findAssignees.js
@@ -0,0 +1,28 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function findAssignees(body) {
+    let result = [];
+    let lines = body.split('\r\n');
+    let m, n;
+    const reg1 = /(@[a-zA-Z0-9\-]+[\s\W]+)+[p|P][t|T][a|A][l|L]\s*/mg;
+    const reg2 = /@([a-zA-Z\d](?:[a-zA-Z\d]|-(?=[a-zA-Z\d])){0,38})/mg;
+    lines.forEach(l => {
+        // console.log("\n  L=" + l);
+        while ((m = reg1.exec(l)) !== null) {
+            // console.log("    M=" + m[0])
+            if (m.index === reg1.lastIndex) {
+                reg1.lastIndex++;
+            }
+            while ((n = reg2.exec(m[0])) !== null) {
+                // console.log("      N=" + n[1])
+                if (n.index === reg2.lastIndex) {
+                    reg2.lastIndex++;
+                }
+                result.push(n[1]);
+            }
+        }
+    });
+    console.log("Assigning these members: " + result);
+    return result;
+}
+exports.findAssignees = findAssignees;
